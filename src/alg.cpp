a73c97b1ecc1cef26acb02ce9488082ba3eb00ab
4yMHouDeZ <sergoart1234@gmail.com>
2026-03-28 20:01:43 +0300

    sergey

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,14 @@
 // Copyright 2021 NNTU-CS
 int countPairs1(int *arr, int len, int value) {
-  return 0;
+  int count = 0;
+  return count;
 }
 int countPairs2(int *arr, int len, int value) {
-  return 0;
+  int count = 0;
+  return count;
 }
+
 int countPairs3(int *arr, int len, int value) {
-  return 0;
+  int count = 0;
+  return count;
 }
